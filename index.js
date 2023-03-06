70342c45b0b1693faf58b8e97815ce942624d8f5
antonijasimic <antonija1023@gmail.com>
2023-03-06 14:33:46 +0100

    add env PORT

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const config = require("./config/app");
-const router = require("./router");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const config = require('./config/app');
+const router = require('./router');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
-const http = require("http");
+const http = require('http');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(router);
-app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/uploads"));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/uploads'));
 
-const port = config.appPort;
+const port = process.env.PORT || config.appPort;
 
 const server = http.createServer(app);
-const SocketServer = require("./socket");
+const SocketServer = require('./socket');
 SocketServer(server);
 
 server.listen(port, () => {
