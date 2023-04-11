4a27d3177512a9372f718ca3a74a8398b8cc62cb
antonijasimic <antonija1023@gmail.com>
2023-04-11 13:46:19 +0200

    remove gender from validators

--- validators/auth/register.js
+++ validators/auth/register.js
@@ -1,11 +1,10 @@
-const { body } = require("express-validator");
+const { body } = require('express-validator');
 
 exports.rules = () => {
   return [
-    body("firstName").notEmpty(),
-    body("lastName").notEmpty(),
-    body("gender").notEmpty(),
-    body("email").isEmail(),
-    body("password").isLength({ min: 5 }),
+    body('firstName').notEmpty(),
+    body('lastName').notEmpty(),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
   ];
 };
