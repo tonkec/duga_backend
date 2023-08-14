f5b9a4f723fd2d96752773ac655adb030ea6ce39
antonijasimic <antonija1023@gmail.com>
2023-08-14 09:30:37 +0200

    transform s3 images

--- router/uploads.js
+++ router/uploads.js
@@ -1,50 +1,72 @@
 const { auth } = require('../middleware/auth');
 const router = require('express').Router();
 require('dotenv').config();
-const { S3Client, ListObjectsCommand } = require('@aws-sdk/client-s3');
+const AWS = require('aws-sdk');
 const multer = require('multer');
-const multerS3 = require('multer-s3');
-const path = require('path');
+const multerS3 = require('multer-s3-transform');
 
-let s3 = new S3Client({
-  region: 'eu-north-1',
-  credentials: {
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
-  },
-  sslEnabled: false,
-  s3ForcePathStyle: true,
-  signatureVersion: 'v4',
+const sharp = require('sharp');
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
 });
 
-const upload = multer({
-  limits: { fileSize: 500000 },
-  fileFilter: function (req, file, callback) {
-    var ext = path.extname(file.originalname);
-    if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-      return callback(new Error('Only images are allowed'));
-    }
-    callback(null, true);
-  },
-  storage: multerS3({
-    s3: s3,
-    bucket: 'duga-user-photo',
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key: function (req, file, cb) {
-      cb(null, `user/${req.user.id}/${Date.now().toString()}`);
-    },
-  }),
-});
+const s3 = new AWS.S3();
+
+const upload = (s3) =>
+  multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: 'duga-user-photo',
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      shouldTransform: function (req, file, cb) {
+        cb(null, /^image/i.test(file.mimetype));
+      },
+      transforms: [
+        {
+          id: 'original',
+          key: function (req, file, cb) {
+            cb(
+              null,
+              `user/${req.user.id}/${Date.now().toString()}/${
+                file.originalname
+              }`
+            );
+          },
+          transform: function (req, file, cb) {
+            cb(null, sharp().resize(600, 600));
+          },
+        },
+        {
+          id: 'thumbnail',
+          key: function (req, file, cb) {
+            cb(
+              null,
+              `user/${
+                req.user.id
+              }/${Date.now().toString()}/${`thumbnail-${file.originalname}`}`
+            );
+          },
+          transform: function (req, file, cb) {
+            cb(null, sharp().resize(100, 100));
+          },
+        },
+      ],
+    }),
+  });
 
 const getImages = async (req, res) => {
+  const params = {
+    Bucket: 'duga-user-photo',
+    Prefix: `user/${req.params.id}/`,
+  };
+
   try {
-    const data = await s3.send(
-      new ListObjectsCommand({
-        Bucket: 'duga-user-photo',
-        Prefix: `user/${req.user.id}`,
-      })
-    );
-    return res.json(data);
+    const data = await s3.listObjectsV2(params).promise();
+    const contents = data.Contents;
+
+    return res.status(200).json(contents);
   } catch (e) {
     return res.status(500).json({ message: e.message });
   }
@@ -52,7 +74,7 @@ const getImages = async (req, res) => {
 
 router.post(
   '/avatar',
-  [auth, upload.array('avatar', 1)],
+  [auth, upload(s3).array('avatar', 1)],
   function (req, res, next) {
     try {
       return res.send('Successfully uploaded ' + req.files.length + ' files!');
