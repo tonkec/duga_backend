20ea322009bfdcf948453d5aabc48e45b360382a
antonijasimic <antonija1023@gmail.com>
2023-08-14 19:09:44 +0200

    delete s3 images

--- router/uploads.js
+++ router/uploads.js
@@ -72,12 +72,62 @@ const getImages = async (req, res) => {
   }
 };
 
+router.post('/delete-avatar/', [auth], async (req, res) => {
+  // delete user folder by user id and timestamp from Key
+  const params = {
+    Bucket: 'duga-user-photo',
+    Prefix: `user/${req.user.id}/`,
+  };
+
+  try {
+    const data = await s3.listObjectsV2(params).promise();
+    const contents = data.Contents;
+
+    const objects = contents.map((content) => {
+      return { Key: content.Key };
+    });
+
+    const filteredObjects = objects.filter((object) => {
+      if (object.Key === req.body.item.Key) {
+        return true;
+      }
+      const thumbnail = req.body.item.Key.substring(
+        0,
+        req.body.item.Key.lastIndexOf('/') + 1
+      ).concat(
+        'thumbnail-',
+        req.body.item.Key.substring(req.body.item.Key.lastIndexOf('/') + 1)
+      );
+
+      if (object.Key === thumbnail) {
+        return true;
+      }
+
+      return false;
+    });
+
+    const deleteParams = {
+      Bucket: 'duga-user-photo',
+      Delete: {
+        Objects: filteredObjects,
+      },
+    };
+
+    await s3.deleteObjects(deleteParams).promise();
+
+    return res.status(200).json({ message: 'Avatar deleted' });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: e.message });
+  }
+});
+
 router.post(
   '/avatar',
   [auth, upload(s3).array('avatar', 1)],
   function (req, res, next) {
     try {
-      return res.send('Successfully uploaded ' + req.files.length + ' files!');
+      return res.send(res.req.files);
     } catch (e) {
       return res.status(500).json({ message: e.message });
     }
