41405be2c26f897741263f7487340d3bac956285
tonkec <antonija1023@gmail.com>
2025-01-01 11:45:12 +0100

    remove env vars

--- config/database.js
+++ config/database.js
@@ -21,10 +21,6 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
     dialect: 'postgres',
     protocol: 'postgres',
     ssl: true,
