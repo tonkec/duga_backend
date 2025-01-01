dd05532664bbeef262baca2b0b02ee88646b8d56
tonkec <antonija1023@gmail.com>
2025-01-01 11:13:37 +0100

    debug timeout

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,12 +12,15 @@ const {
 
 exports.login = async (req, res) => {
   const { password, email } = req.body;
+  console.log(req.body);
   try {
     const user = await User.findOne({
       where: {
         email: email,
       },
     });
+
+    console.log(user);
 
     if (!user) return res.status(404).json({ message: 'User not found' });
 
