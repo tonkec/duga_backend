bf8da42e64d8f91cd0691654e96799cefbd6a833
tonkec <antonija1023@gmail.com>
2025-01-01 11:27:02 +0100

    improve configs

--- config/database.js
+++ config/database.js
@@ -20,14 +20,18 @@ module.exports = {
     logging: false,
   },
   production: {
+    use_env_variable: 'DATABASE_URL',
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
     logging: true,
     dialectOptions: {
       ssl: {
+        require: true,
         rejectUnauthorized: false,
       },
     },
