57d6ad5f6327ac456aff6d24c8e550e3cf439e56
tonkec <antonija1023@gmail.com>
2025-01-14 12:09:45 +0100

    setup db for heroku

--- config/database.js
+++ config/database.js
@@ -7,6 +7,10 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: false,
+      native: true
+    } 
   },
   test: {
     username: process.env.DB_USER,
@@ -14,19 +18,13 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    
   },
   production: {
+    username: DB_USERNAME_PROD,
+    password: DB_PASSWORD_PROD,
+    database: DB_NAME_PROD,
+    host: DB_HOST_PROD,
     dialect: 'postgres',
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.APP_PORT,
-    dialectOptions: {
-      ssl: {
-        require: true, 
-        rejectUnauthorized: false, 
-      },
-    },
-  },
+  }
 };
