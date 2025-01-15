a00855b6218576003d2efd402850f27aeb7a22a7
tonkec <antonija1023@gmail.com>
2025-01-15 09:24:09 +0100

    add staging configs

--- config/database.js
+++ config/database.js
@@ -32,5 +32,18 @@ module.exports = {
         rejectUnauthorized: false
       }
     }
+  },
+  staging: {
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
