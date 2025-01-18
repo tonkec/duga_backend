cc22cf323e3b3d6c43dbf0c6fb12d1d89a14d506
tonkec <antonija1023@gmail.com>
2025-01-18 09:09:13 +0100

    revert sender

--- helpers/emailHelpers/generateMessage.js
+++ helpers/emailHelpers/generateMessage.js
@@ -2,7 +2,7 @@ exports.generateMessage = (token, toUser, subject, text, url, html, port) => {
   const hostUrl = `${process.env.APP_URL}`;
   const msg = {
     to: toUser,
-    from: 'antonija1023@gmail.com',
+    from: 'admin@duga.app',
     subject: subject,
     text: text,
     html: `${html}: ${hostUrl}:${port}/${url}?token=${token}&email=${toUser}`,
