667fd6f3b1f111316c4b1e7c363de85296f598f8
tonkec <antonija1023@gmail.com>
2025-02-27 13:58:53 +0100

    feat(auth0): remove unused endpoints

--- router/auth.js
+++ router/auth.js
@@ -1,31 +1,11 @@
 const router = require('express').Router();
 const {
-  login,
   register,
-  forgotPassword,
-  resetPassword,
 } = require('../controllers/authController');
-const {
-  verify,
-  getVerificationToken,
-} = require('../controllers/verificationController');
+
 const { validate } = require('../validators/index');
-const { rules: registrationRules } = require('../validators/auth/register');
-const { rules: loginRules } = require('../validators/auth/login');
-const {
-  rules: forgotPasswordRules,
-} = require('../validators/auth/forgotPassword');
 
-router.post('/login', [loginRules(), validate], login);
 router.post('/register', [registrationRules(), validate], register);
-router.get('/verification', verify);
-router.post(
-  '/forgot-password',
-  [forgotPasswordRules(), validate],
-  forgotPassword
-);
 
-router.post('/reset-password', resetPassword);
-router.post('/verification-token', getVerificationToken);
 
 module.exports = router;
