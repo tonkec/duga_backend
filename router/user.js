46aba4ddee7d7300719d48f89f900182d0584ee5
tonkec <antonija1023@gmail.com>
2025-04-16 13:53:31 +0200

    bug(online-status): use correct method

--- router/user.js
+++ router/user.js
@@ -3,12 +3,13 @@ const {
   update,
   getAllUsers,
   getUser,
-  getUsersByUsername
+  getUsersByUsername,
+  getUserOnlineStatus,
 } = require('../controllers/usersController');
 const { checkJwt } = require('../middleware/auth');
 router.post('/update-user', [checkJwt], update);
 router.get('/get-users', checkJwt, getAllUsers);
 router.get('/username/:username', checkJwt, getUsersByUsername); 
 router.get('/:id', checkJwt, getUser);
-router.get("/online-status/:id/", checkJwt, getUser);
+router.get("/online-status/:id/", checkJwt, getUserOnlineStatus);
 module.exports = router;
