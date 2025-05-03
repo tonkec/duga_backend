10da7b36bc1049d5f8d12ad99fc72b1039f2d147
Antonija Šimić <antonija1023@gmail.com>
2025-05-03 08:01:21 +0200

    bug(new-user-login): use findOrCreate (#15)

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,16 +8,18 @@ const MANAGEMENT_API_AUDIENCE = `https://${AUTH0_DOMAIN}/api/v2/`;
 exports.register = async (req, res) => {
   const { email } = req.body;
   try {
-    let user = await User.findOne({ where: { email } });
-    if (!user) {
-      user = await User.create(req.body);
+    const [user, created] = await User.findOrCreate({
+      where: { email },
+      defaults: req.body,
+    });
 
+    if (created) {
       res.status(201).json({ message: 'User created', user });
     } else {
       res.status(200).json({ message: 'User already exists', user });
     }
   } catch (error) {
-    console.error('Error creating user:', error);
+    console.error('Error registering user:', error);
     res.status(500).json({ message: 'Error creating user' });
   }
 };
