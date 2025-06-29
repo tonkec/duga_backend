1f663835d0c494a5393028b40cc8a65c687d69f7
Antonija Šimić <antonija1023@gmail.com>
2025-06-29 09:33:38 +0200

    Staging (#23)
    * bug(mark-all-as-read): fix property name (#21)
    
    * Security/upload file types (#20)
    
    * security(upload-file-types): allow ony specific file types for comment uploads
    
    * security(upload-file-types): allow ony specific file types for user profile photos
    
    * security(upload-file-types): allow ony specific file types for chat photos

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -3,6 +3,7 @@ const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3-transform');
 const sharp = require('sharp');
+const allowedMimeTypes = require("../consts/allowedFileTypes")
 
 AWS.config.update({
   accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
@@ -62,6 +63,15 @@ exports.uploadMessageImage = s3 => {
         },
       ],
     }),
+    fileFilter: (req, file, cb) => {
+      if (allowedMimeTypes.includes(file.mimetype)) {
+        cb(null, true);
+      } else {
+        const error = new Error('Invalid file type. Only PNG, JPG, JPEG, and SVG are allowed.');
+        error.code = 'INVALID_FILE_TYPE';
+        cb(error);
+      }
+    },
   });  
 }
 
@@ -111,6 +121,13 @@ exports.uploadMultiple = (s3) => {
         },
       ],
     }),
+    fileFilter: (req, file, cb) => {
+      if (allowedMimeTypes.includes(file.mimetype)) {
+        cb(null, true);
+      } else {
+        cb(new Error('Invalid file type. Only PNG, JPG, JPEG, and SVG are allowed.'));
+      }
+    },
   });
 };
 
