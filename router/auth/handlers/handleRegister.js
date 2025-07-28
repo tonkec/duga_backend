2bbd0e020f941761d6c0c30f2eddaeebfe07cba5
tonkec <antonija1023@gmail.com>
2025-07-28 08:53:38 +0200

    feature(random-username): save username to db

--- router/auth/handlers/handleRegister.js
+++ router/auth/handlers/handleRegister.js
@@ -1,17 +1,18 @@
 
 const { User } = require('../../../models');
 
-const register = async (req, res) => {
-  const { auth0Id, email } = req.body;
+const handleRegister = async (req, res) => {
+  const { auth0Id, email, username } = req.body;
 
-  if (!auth0Id || !email) {
+  if (!auth0Id || !email || !username) {
     return res.status(400).json({ message: 'Missing required fields' });
   }
 
   try {
-      const normalizedEmail = email.toLowerCase();
+    const normalizedEmail = email.toLowerCase();
 
     let user = await User.findOne({ where: { email: normalizedEmail } });
+
     if (user) {
       if (!user.auth0Id) {
         await user.update({ auth0Id });
@@ -20,13 +21,19 @@ const register = async (req, res) => {
       return res.status(200).json({ message: 'User already exists', user });
     }
 
-    user = await User.create({ auth0Id, email: normalizedEmail, });
-    return res.status(201).json({ message: 'User created', user });
+    user = await User.create({
+      auth0Id,
+      email: normalizedEmail,
+      username, 
+    });
 
+    console.log('User created');
+    return res.status(201).json({ message: 'User created', user });
   } catch (error) {
     console.error('Error registering user:', error);
     res.status(500).json({ message: 'Error creating user' });
   }
 };
 
-module.exports = register;
+
+module.exports = handleRegister;
